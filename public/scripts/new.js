66c976db35b114ab21248091a0c3f90d4db0486d
BRAD FOLKERS <BRADLEYDFOLKERS@GMAIL.COM>
2017-03-17 11:10:48 -0600

    Finished edit page

--- public/scripts/new.js
+++ public/scripts/new.js
@@ -7,12 +7,12 @@ $(function() {
     ++ingredientCount;
     $('.recipe-ingredients').append(`
       <div class="row">
-        <label for="recipe-ingredient-quantity" class="col-md-1">Quantity: </label>
-        <div class="col-md-2">
+        <label for="recipe-ingredient-quantity" class="col-lg-1 col-md-1 col-sm-1 col-xs-1">Quantity: </label>
+        <div class="col-lg-2 col-md-2 col-sm-2 col-xs-2">
           <input type="text" class="recipe-ingredient-quantity form-control" id="recipe-ingredient-quantity-${ingredientCount.toString()}" placeholder="Quantity">
         </div>
-        <label for="recipe-ingredient-name" class="col-md-1">Ingredient: </label>
-        <div class="col-md-8">
+        <label for="recipe-ingredient-name" class="col-lg-1 col-md-1 col-sm-1 col-xs-1">Ingredient: </label>
+        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8">
           <input type="text" class="recipe-ingredient-name form-control" id="recipe-ingredient-name-${ingredientCount.toString()}" placeholder="Ingredient">
         </div>
       </div>
@@ -20,7 +20,7 @@ $(function() {
   })
 
   $('.add-recipe-step-button').on('click', function(event) {
-    // event.preventDefault();
+    event.preventDefault();
     ++stepCount;
     $('.recipe-steps').append(`
       <label for="recipe-step" class="col-md-2 control-label">Step ${stepCount.toString()}: </label>
@@ -43,21 +43,71 @@ $(function() {
   })
 
   $('.submit-button').on('click', function(event) {
+    //1. Author table- get back author_id
+    //  name
+    //  /author
+    //2. Recipe table- using author_id, get back recipe_id
+    //  title, image, description, author_id
+    //  /recipeAdd
+    //3. Step table- using recipe_id, plus step number, and step body
+    //  step_number, step_body, id(a.k.a. recipe_id)
+    //  /stepAdd
+    //4. Ingredient table-
+    //  name, id (a.k.a. recipe_id), quantity
+    //  /ingredientAdd
     event.preventDefault();
     var recipe = {};
-    recipe.name = $('#inputTitle').val();
+    recipe.id = null;
+    recipe.name = $('#inputAuthor').val();
+    recipe.author_id = null;
+    recipe.title = $('#inputTitle').val();
     recipe.image = $('#inputImage').val();
+    recipe.description = $('#inputDescription').val();
     recipe.steps = [];
     recipe.ingredients = [];
-    for (var i = 0; i < stepCount; i++) {
-      recipe.steps[i] = $('#recipeStep-' + (i + 1)).val();
-    }
-    for (var i = 0; i < ingredientCount; i++) {
-      recipe.ingredients[i] = {quantity: $('#recipe-ingredient-quantity-' + (i + 1)).val(),
-                               name: $('#recipe-ingredient-name-' + (i + 1)).val()};
-    }
-    console.log(recipe.steps);
-     // window.location.replace("./index.html");
-  })
+    $.post('https://g-recipies.herokuapp.com/author', {name: recipe.name})
+      .then(function(authorId) {
+        recipe.author_id = authorId;
+        $.post('https://g-recipies.herokuapp.com/recipeAdd', {
+          title: recipe.title, image: recipe.image, description: recipe.description, author_id: recipe.author_id})
+          .then(function(recipeId) {
+            recipe.id = recipeId;
+            var recipeSteps = [];
+            for (var i = 0; i < stepCount; i++) {
+              recipeSteps.push({step_number: i + 1, step_body: $('#recipeStep-' + (i + 1)).val(), id: recipeId});
+            }
+            for (let i = 0; i < stepCount; i++) {
+              $.post('https://g-recipies.herokuapp.com/stepAdd', recipeSteps[i])
+                .then(function(data, status) {
+                  console.log(status);
+                })
+                .catch(function(error) {
+                  console.log(error);
+                })
+            }
+            var ingredients = [];
+            for (var j = 0; j < ingredientCount; j++) {
+              ingredients.push({quantity: $('#recipe-ingredient-quantity-' + (j + 1)).val(),
+                                       name: $('#recipe-ingredient-name-' + (j + 1)).val(), id: recipeId});
+            }
+            for (let j = 0; j < ingredientCount; j++) {
+              $.post('https://g-recipies.herokuapp.com/ingredientAdd', ingredients[j])
+                .then(function(data, status) {
+                  console.log(status);
+                })
+                .catch(function(error) {
+                  console.log(error);
+                })
+            }
 
+          })
+          .catch(function(error) {
+            console.log(error);
+          })
+      })
+      .catch(function(data, status) {
+        console.log(status);
+      })
+     window.location.replace("./index.html");
+  })
 })
--- public/scripts/recipe.js
+++ public/scripts/recipe.js
@@ -12,12 +12,50 @@ $(function() {
       var thisRecipe = recipes.filter(function(element) {
         return (element.id === Number(recipeId));
       })
+      var recipeName = thisRecipe[0].name;
+      var recipeNameArray = recipeName.split(' ');
+      for (var i in recipeNameArray) {
+        var word = recipeNameArray[i].toLowerCase();
+        if (word === 'and' || word === 'with') {
+          recipeNameArray[i] = recipeNameArray[i].toLowerCase();
+        } else {
+          recipeNameArray[i] = recipeNameArray[i].toLowerCase();
+          recipeNameArray[i] = capitalizeFirstLetter(recipeNameArray[i]);
+        }
+      }
+      var formattedRecipeName = recipeNameArray.join(' ');
       var thisRating = thisRecipe[0].avg;
-      $('.name-field').text(thisRecipe[0].name);
+      $('.name-field').text(formattedRecipeName);
       $('.description-field').text(data[0].description);
       $('.rating-field').text(Number(thisRating).toFixed(1).toString());
       $('.author-field').text(data[0].name);
       $('.food-pic').attr('src', thisRecipe[0].image);
       $('.edit-button').attr('href', './edit.html?recipe=' + recipeId);
     })
+
+  $.get('https://g-recipies.herokuapp.com/indivRecipeIngred/' + recipeId)
+    .then(function(data, status) {
+      var $ingredientList = $('.ingredient-list');
+      for (var i in data) {
+        $ingredientList.append(`<li>${data[i].quantity} ${data[i].name}</li>`)
+      }
+    })
+    .catch(function(error) {
+      console.log(error);
+    })
+
+  $.get('https://g-recipies.herokuapp.com/indivRecipeSteps/' + recipeId)
+    .then(function(recipeSteps) {
+      var sortedRecipeSteps = recipeSteps.sort(function(a, b) {
+        return a.step_number - b.step_number;
+      })
+      var $stepList = $('.step-list');
+      for (var i in sortedRecipeSteps) {
+        $stepList.append(`<li>${sortedRecipeSteps[i].step_number}. ${sortedRecipeSteps[i].step_body}</li>`);
+      }
+    })
 })
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
